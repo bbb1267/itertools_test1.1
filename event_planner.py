536e9a48bbcfce81dd270a299966abb75c627a62
bbb1267 <hshshss340@gmail.com>
2026-02-09 22:47:13 +0300

    Update event_planner.py
    Feat: add cyclic DJ queue and seating logic

--- event_planner.py
+++ event_planner.py
@@ -12,4 +12,15 @@ def run_event_planner():
 
     # 2. Рассадка по парам (combinations)
     pairs = list(itertools.combinations(guests, 2))
-    print(f"Пар для нетворкинга: {pairs}"
+    print(f"Пар для нетворкинга: {pairs}")
+
+    #3. Дежурство (cycle + islice)
+    dj_queue = itertools.cycle(guests)
+    first_10_djs = list(itertools.islice(dj_queue, 10))
+    print(f"Очередь DJ: {first_10_djs}")
+
+    #4. Группировка по столам(zip_longest)
+    seating = list(itertools.zip_longest(guests, table_numbers, fillvalue="Без места"))
+    print(f"Рассадка: {seating}")
+
+          
